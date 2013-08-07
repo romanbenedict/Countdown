e278e8b17633c040c4c6f78819ae41e974630f36
Roman Benedict <romanbenedict@gmail.com>
2013-08-07 16:59:06 +0100

    Update with styling, PHP get requests and more

--- countdown.php
+++ countdown.php
@@ -1,25 +1,48 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Countdown to ...</title>
+    <title>Countdown to ... <?php if(isset($_GET['title'])){
+echo $_GET['title'];}else;?></title>
     <!-- Styles -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	<style type="text/css">@import "js/jquery.countdown.css";</style> 
 	<link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Arbutus+Slab' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Kristi' rel='stylesheet' type='text/css'>
+	<link href="css/countdown.css" rel="stylesheet">
+	<!--Scripts-->
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.countdown.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
   </head>
   <body>
 	<div class="container">
 	<!-- Header-->
-    <h1 style="font-family: 'Arbutus Slab', serif; text-align:center">Countdown to <span class="editable" style="font-family: 'Kristi', cursive;">[...]</span></h1>
-	<!--Countdowns-->
-	<h2 style="font-family: 'Kristi', cursive; text-align:center;"><span id="countingto" >Date </span> <a href="#" id="todatepicker" data-date-format="yyyy-mm-dd" data-date="<?php echo date("Y-m-d");?>"><i class="icon-calendar"></i></a></h2>
-	<div class="row"><div id="countdownto" class="well span8 offset2"><span class="countdown_row countdown_show4"><span class="countdown_section"><span class="countdown_amount">--</span><br>Week</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Days</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Hours</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Minutes</span></span></div></div>
-	<h1 style="font-family: 'Arbutus Slab', serif; text-align:center">Time From <span id="countingfrom" style="font-family: 'Kristi', cursive;">Date </span> <a href="#" id="fromdatepicker" data-date-format="yyyy-mm-dd" data-date="2012-09-20"><i class="icon-calendar"></i></a></h1>
-	<div class="row"><div id="countdownfrom" class="well span8 offset2"><span class="countdown_row countdown_show4"><span class="countdown_section"><span class="countdown_amount">--</span><br>Week</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Days</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Hours</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Minutes</span></span></div></div>
+    <h1 style="" id="topheader">Countdown to <span class="editable" style="font-family: 'Kristi', cursive;"><?php if(isset($_GET['title'])){
+echo $_GET['title'];}else{echo "[...]";};?></span></h1>
+	<!--Countdown1-->
+	<h2 style="font-family: 'Kristi', cursive; text-align:center;"><span id="countingto" class="daterender"><?php if(isset($_GET['start'])){
+echo $_GET['start'];}else{echo "Date";};?></span> <a href="#" id="todatepicker" data-date-format="yyyy-mm-dd" data-date="<?php if(isset($_GET['start'])){
+echo $_GET['start'];}else{echo date("Y-m-d");};?>"><i class="icon-calendar"></i></a></h2>
+	<div class="row">
+	<?php if(isset($_GET['start'])){ 
+echo "<script>\$(document).ready(function () {\$('#countdownto').countdown({until: new Date('".$_GET['start']."'), format: 'wdHM'})});</script><div id='countdownto' class='well span8 offset2'>";
+}
+else{ 
+print	"<div id='countdownto' class='well span8 offset2'><span class='countdown_row countdown_show4'><span class='countdown_section'><span class='countdown_amount'>--</span><br>Week</span><span class='countdown_section'><span class='countdown_amount'>--</span><br>Days</span><span class='countdown_section'><span class='countdown_amount'>--</span><br>Hours</span><span class='countdown_section'><span class='countdown_amount'>--</span><br>Minutes</span></span>";
+};?>
+	</div></div>
+	<!--Countdown 2-->
+	<h1 style="font-family: 'Arbutus Slab', serif; text-align:center" id="timefrom">Time From <span id="countingfrom" style="font-family: 'Kristi', cursive;" class="daterender"><?php if(isset($_GET['end'])){
+echo $_GET['end'];}else{echo "Date";};?>Date </span> <a href="#" id="fromdatepicker" data-date-format="yyyy-mm-dd" data-date="<?php if(isset($_GET['end'])){
+echo $_GET['end'];}else{echo date("Y-m-d");};?>"><i class="icon-calendar"></i></a></h1>
+	<div class="row">
+	<div id="countdownfrom" class="well span8 offset2">
+	<span class="countdown_row countdown_show4"><span class="countdown_section"><span class="countdown_amount">--</span><br>Week</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Days</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Hours</span><span class="countdown_section"><span class="countdown_amount">--</span><br>Minutes</span></span>
+	</div></div>
 	<!--Progress Bar-->
-	<div class="row"><div class="span1">Start</div><div id="progressbarcontainer" class="progress span10"><!--<div class="bar" style="width: 0%;"></div>--></div><div class="span1">End</div></div>
+	<div class="row" id="progressbar"><div class="span1 progresslabel">Start</div><div id="progressbarcontainer" class="progress span10"><!--<div class="bar" style="width: 0%;"></div>--></div><div class="span1 progresslabel">End</div></div>
 	<!--Presets Row-->
 	<hr>
 	<div class="row"><a class="span2 btn btn-large">New Year</a><a class="span2 btn btn-large">Easter</a><a class="span2 btn btn-large">Christmas</a><a class="span2 btn btn-large">Mayan Apocalypse</a><a class="span2 btn btn-large">Save for Bookmark</a></div>
@@ -40,11 +63,7 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 	<hr>
 	<p style="text-align:center; text-size:small;">Copyright &copy Roman Benedict 2012 / Version 0.5</p>
    </div>
-   <!--Scripts-->
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.countdown.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
+   
 	<!--Datepicker Scripts-->
 	<script>
 	$('#todatepicker').datepicker()
